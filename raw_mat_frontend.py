d9bc4e1a5ddbe0599bfbae3fb3306e330c0f9b2d
alex <gominsan@gmail.com>
2025-08-17 08:30:58 +0700

    commit 11

--- raw_mat_frontend.py
+++ raw_mat_frontend.py
@@ -1,5 +1,6 @@
 from nicegui import ui
 from raw_mat_widget import rawMatWidget
+from raw_mat_widget import infoPanel
 
 class rawmaterial_panel():
     def __init__(self, backend_model, ip_addr, port):
@@ -9,56 +10,74 @@ class rawmaterial_panel():
         self.pincode = ''
 
 
-    def init_frontend(self):
+    def widgets_stack(self):
+        wi_w = 200
+        with ui.row().classes("w-full"):
+            self.stack_group_solvents = ui.label('Solvents and reagents:').style('font-size: 30px;')
+
+        with ui.grid(columns=7).classes("w-full").style(f"grid-template-columns: "
+                                                        f"{wi_w}px {wi_w}px {wi_w}px {wi_w}px {wi_w}px {wi_w}px {wi_w}px"):
+            rawMatWidget(self.ip_addr, self.port, 'INIT_BASE_CODE_OLIGO_LAB_0000001', self.pincode)
+            rawMatWidget(self.ip_addr, self.port, 'INIT_BASE_CODE_OLIGO_LAB_0000007', self.pincode)
+            rawMatWidget(self.ip_addr, self.port, 'INIT_BASE_CODE_OLIGO_LAB_0000004', self.pincode)
+            rawMatWidget(self.ip_addr, self.port, 'INIT_BASE_CODE_OLIGO_LAB_0000010', self.pincode)
+            rawMatWidget(self.ip_addr, self.port, 'INIT_BASE_CODE_OLIGO_LAB_0000018', self.pincode)
+            rawMatWidget(self.ip_addr, self.port, 'INIT_BASE_CODE_OLIGO_LAB_0000009', self.pincode)
+            rawMatWidget(self.ip_addr, self.port, 'INIT_BASE_CODE_OLIGO_LAB_0000008', self.pincode)
+            rawMatWidget(self.ip_addr, self.port, 'INIT_BASE_CODE_OLIGO_LAB_0000006', self.pincode)
+            rawMatWidget(self.ip_addr, self.port, 'INIT_BASE_CODE_OLIGO_LAB_0000011', self.pincode)
+
+        with ui.row().classes("w-full"):
+            self.stack_group_solvents = ui.label('Phosphoramidites:').style('font-size: 30px;')
+
         wi_w = 200
 
+        with ui.grid(columns=7).classes("w-full").style(f"grid-template-columns: "
+                                                        f"{wi_w}px {wi_w}px {wi_w}px {wi_w}px {wi_w}px {wi_w}px {wi_w}px"):
+            rawMatWidget(self.ip_addr, self.port, 'INIT_BASE_CODE_OLIGO_LAB_0000120', self.pincode)
+            rawMatWidget(self.ip_addr, self.port, 'INIT_BASE_CODE_OLIGO_LAB_0000121', self.pincode)
+            rawMatWidget(self.ip_addr, self.port, 'INIT_BASE_CODE_OLIGO_LAB_0000122', self.pincode)
+            rawMatWidget(self.ip_addr, self.port, 'INIT_BASE_CODE_OLIGO_LAB_0000123', self.pincode)
+            rawMatWidget(self.ip_addr, self.port, 'INIT_BASE_CODE_OLIGO_LAB_0000129', self.pincode)
+            rawMatWidget(self.ip_addr, self.port, 'INIT_BASE_CODE_OLIGO_LAB_0000133', self.pincode)
+            rawMatWidget(self.ip_addr, self.port, 'INIT_BASE_CODE_OLIGO_LAB_0000130', self.pincode)
+            rawMatWidget(self.ip_addr, self.port, 'INIT_BASE_CODE_OLIGO_LAB_0000124', self.pincode)
+            rawMatWidget(self.ip_addr, self.port, 'INIT_BASE_CODE_OLIGO_LAB_0000125', self.pincode)
+            rawMatWidget(self.ip_addr, self.port, 'INIT_BASE_CODE_OLIGO_LAB_0000126', self.pincode)
+            rawMatWidget(self.ip_addr, self.port, 'INIT_BASE_CODE_OLIGO_LAB_0000127', self.pincode)
+
+        with ui.row().classes("w-full"):
+            self.stack_group_solvents = ui.label('Azides:').style('font-size: 30px;')
+
+        wi_w = 200
+
+        with ui.grid(columns=7).classes("w-full").style(f"grid-template-columns: "
+                                                        f"{wi_w}px {wi_w}px {wi_w}px {wi_w}px {wi_w}px {wi_w}px {wi_w}px"):
+            rawMatWidget(self.ip_addr, self.port, 'INIT_BASE_CODE_OLIGO_LAB_0000166', self.pincode)
+            rawMatWidget(self.ip_addr, self.port, 'INIT_BASE_CODE_OLIGO_LAB_0000167', self.pincode)
+            rawMatWidget(self.ip_addr, self.port, 'INIT_BASE_CODE_OLIGO_LAB_0000168', self.pincode)
+            rawMatWidget(self.ip_addr, self.port, 'INIT_BASE_CODE_OLIGO_LAB_0000170', self.pincode)
+            rawMatWidget(self.ip_addr, self.port, 'INIT_BASE_CODE_OLIGO_LAB_0000171', self.pincode)
+            rawMatWidget(self.ip_addr, self.port, 'INIT_BASE_CODE_OLIGO_LAB_0000172', self.pincode)
+
+    def init_frontend(self):
         if self.pincode != '':
-            with ui.row().classes("w-full"):
-                self.stack_group_solvents = ui.label('Solvents and reagents:').style('font-size: 30px;')
+            with (((ui.grid(columns=2).classes("w-full").style(f"grid-template-columns: {1550}px {1200}px")))):
+                with ui.column():
+                    self.widgets_stack()
+                with ui.column():
+                    ui.label('Info panel:').style('font-size: 30px;')
+                    self.info_panel = infoPanel(self.ip_addr, self.port, self.pincode)
+                    with ui.row():
+                        ui.label('Search by name:').style('font-size: 30px;')
+                        self.search_text = ui.input(label='', placeholder='Search name',
+                                                    on_change=self.info_panel.search_by_name).style('font-size: 30px;').classes('w-[700px]')
+                        #self.on_search = ui.button('Search', color="#00a100").classes('w-[200px]')
 
-            with ui.grid(columns=7).classes("w-full").style(f"grid-template-columns: "
-                                        f"{wi_w}px {wi_w}px {wi_w}px {wi_w}px {wi_w}px {wi_w}px {wi_w}px"):
-                rawMatWidget(self.ip_addr, self.port, 'INIT_BASE_CODE_OLIGO_LAB_0000001', self.pincode)
-                rawMatWidget(self.ip_addr, self.port, 'INIT_BASE_CODE_OLIGO_LAB_0000007', self.pincode)
-                rawMatWidget(self.ip_addr, self.port, 'INIT_BASE_CODE_OLIGO_LAB_0000004', self.pincode)
-                rawMatWidget(self.ip_addr, self.port, 'INIT_BASE_CODE_OLIGO_LAB_0000010', self.pincode)
-                rawMatWidget(self.ip_addr, self.port, 'INIT_BASE_CODE_OLIGO_LAB_0000018', self.pincode)
-                rawMatWidget(self.ip_addr, self.port, 'INIT_BASE_CODE_OLIGO_LAB_0000009', self.pincode)
-                rawMatWidget(self.ip_addr, self.port, 'INIT_BASE_CODE_OLIGO_LAB_0000008', self.pincode)
-                rawMatWidget(self.ip_addr, self.port, 'INIT_BASE_CODE_OLIGO_LAB_0000006', self.pincode)
-                rawMatWidget(self.ip_addr, self.port, 'INIT_BASE_CODE_OLIGO_LAB_0000011', self.pincode)
+                    #self.search_text.on_value_change = self.info_panel.search_by_name
 
-            with ui.row().classes("w-full"):
-                self.stack_group_solvents = ui.label('Phosphoramidites:').style('font-size: 30px;')
 
-            wi_w = 200
 
-            with ui.grid(columns=7).classes("w-full").style(f"grid-template-columns: "
-                                        f"{wi_w}px {wi_w}px {wi_w}px {wi_w}px {wi_w}px {wi_w}px {wi_w}px"):
-                rawMatWidget(self.ip_addr, self.port, 'INIT_BASE_CODE_OLIGO_LAB_0000120', self.pincode)
-                rawMatWidget(self.ip_addr, self.port, 'INIT_BASE_CODE_OLIGO_LAB_0000121', self.pincode)
-                rawMatWidget(self.ip_addr, self.port, 'INIT_BASE_CODE_OLIGO_LAB_0000122', self.pincode)
-                rawMatWidget(self.ip_addr, self.port, 'INIT_BASE_CODE_OLIGO_LAB_0000123', self.pincode)
-                rawMatWidget(self.ip_addr, self.port, 'INIT_BASE_CODE_OLIGO_LAB_0000129', self.pincode)
-                rawMatWidget(self.ip_addr, self.port, 'INIT_BASE_CODE_OLIGO_LAB_0000133', self.pincode)
-                rawMatWidget(self.ip_addr, self.port, 'INIT_BASE_CODE_OLIGO_LAB_0000130', self.pincode)
-                rawMatWidget(self.ip_addr, self.port, 'INIT_BASE_CODE_OLIGO_LAB_0000124', self.pincode)
-                rawMatWidget(self.ip_addr, self.port, 'INIT_BASE_CODE_OLIGO_LAB_0000125', self.pincode)
-                rawMatWidget(self.ip_addr, self.port, 'INIT_BASE_CODE_OLIGO_LAB_0000126', self.pincode)
-                rawMatWidget(self.ip_addr, self.port, 'INIT_BASE_CODE_OLIGO_LAB_0000127', self.pincode)
 
-            with ui.row().classes("w-full"):
-                self.stack_group_solvents = ui.label('Azides:').style('font-size: 30px;')
-
-            wi_w = 200
-
-            with ui.grid(columns=7).classes("w-full").style(f"grid-template-columns: "
-                                        f"{wi_w}px {wi_w}px {wi_w}px {wi_w}px {wi_w}px {wi_w}px {wi_w}px"):
-                rawMatWidget(self.ip_addr, self.port, 'INIT_BASE_CODE_OLIGO_LAB_0000166', self.pincode)
-                rawMatWidget(self.ip_addr, self.port, 'INIT_BASE_CODE_OLIGO_LAB_0000167', self.pincode)
-                rawMatWidget(self.ip_addr, self.port, 'INIT_BASE_CODE_OLIGO_LAB_0000168', self.pincode)
-                rawMatWidget(self.ip_addr, self.port, 'INIT_BASE_CODE_OLIGO_LAB_0000170', self.pincode)
-                rawMatWidget(self.ip_addr, self.port, 'INIT_BASE_CODE_OLIGO_LAB_0000171', self.pincode)
-                rawMatWidget(self.ip_addr, self.port, 'INIT_BASE_CODE_OLIGO_LAB_0000172', self.pincode)
 
 
--- raw_mat_widget.py
+++ raw_mat_widget.py
@@ -1,6 +1,100 @@
 from nicegui import events, ui, app
 from OligoMap_utils import api_db_interface
 import requests
+import pandas as pd
+
+class infoPanel(api_db_interface):
+
+    def __init__(self, api_IP, db_port, pincode):
+        super().__init__(api_IP, db_port)
+        self.pincode = pincode
+        self._bkg = 'infopanel_bkg_2.png'
+
+        self.db_name = 'stock_oligolab_5.db'
+        self.strftime_format = "%Y-%m-%d"
+        self.time_format = "%H:%M:%S"
+
+        self.width = 1000
+        self.height = 800
+        self.search_top = 10
+
+        self.search_color = 'orange'
+        self.selection_color = 'yellow'
+        self.border_color = 'orange'
+
+        self.image = ui.interactive_image(f'/img/{self._bkg}', on_mouse=self.mouse_handler,  # cross='red',
+                                          events=['mousedown', 'mousemove', 'mouseup', 'click'])
+        self.base_layer = self.image.add_layer()
+        self.search_layer = self.image.add_layer()
+        self.search_selection_layer = self.image.add_layer()
+        self.draw()
+
+        self.main_tab_data_df = pd.DataFrame(self.get_all_data_in_tab('total_tab'))
+
+
+    def draw(self):
+        self.base_layer.content = (f'<rect x=0 y=0 width={self.width} height={self.height} '
+                                   f' rx=10 ry=10 fill="none" '
+                                   f'stroke="{self.border_color}" stroke-width="4"/>')
+
+
+    def draw_search_layer(self, df):
+        self.search_layer.content = ""
+        dd = df.to_dict('records')
+        x, y = 20, 10
+        self.search_dict = {}
+        for row in dd:
+            self.search_dict[(x, y, self.width - 20, y + 35)] = row
+
+            self.search_layer.content += (f'<text x="{20}" y="{y + 23}" '
+                                          f'fill="white" font-size="20">{row[1]}</text>')
+
+            self.search_layer.content += (f'<rect x={10} y={y} '
+                                        f'width={self.width - 20} height={35} '
+                                        f'fill="{self.search_color}" fill-opacity="0.2"'
+                                        f'stroke="{self.search_color}" stroke-width="2"/>')
+
+            y += 45
+
+    def draw_search_selection(self, x, y):
+        self.search_selection_layer.content = ""
+        for key in self.search_dict.keys():
+            if (y >= key[1]) and (y <= key[3]):
+                self.selected_row = self.search_dict[key]
+
+                self.search_selection_layer.content += (f'<rect x={10} y={key[1]} '
+                                              f'width={self.width - 20} height={35} '
+                                              f'fill="{self.selection_color}" fill-opacity="0.2"'
+                                              f'stroke="{self.selection_color}" stroke-width="2"/>')
+
+
+    def mouse_handler(self, e: events.MouseEventArguments):
+        x, y = e.image_x, e.image_y
+
+        if e.type == 'mousemove':
+            if self.search_layer.content != "":
+                self.draw_search_selection(x, y)
+
+        if e.type == 'click':
+            pass
+
+
+    def search_by_name(self, e):
+        self.search_dataframe = self.main_tab_data_df[self.main_tab_data_df[1].str.contains(e.value,
+                                                                                            case=False, na=False)]
+        if e.value != '':
+            self.draw_search_layer(self.search_dataframe)
+        else:
+            self.search_layer.content = ""
+
+
+    def get_all_data_in_tab(self, tab_name):
+        url = f'{self.api_db_url}/get_all_tab_data/{self.db_name}/{tab_name}'
+        ret = requests.get(url, headers=self.headers())
+        return ret.json()
+
+
+
 
 class rawMatWidget(api_db_interface):
 
